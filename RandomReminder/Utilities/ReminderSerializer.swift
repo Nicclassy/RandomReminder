a49254705da8fdd7a893272e200d2d021702c094
Nicclassy <lucanapoli122@gmail.com>
2025-01-02 10:09:17 +1100

    Make serialization parameterized and generic

--- RandomReminder/Utilities/ReminderSerializer.swift
+++ RandomReminder/Utilities/ReminderSerializer.swift
@@ -9,27 +9,30 @@ import Foundation
 import SwiftUI
 
 struct ReminderSerializer {
-    typealias CodedReminders = [Reminder]
-    
     static private let remindersFile = "reminders"
     static private let url = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
         .first?
         .appendingPathComponent(remindersFile)
         .appendingPathExtension("json")
     
-    static func load() -> CodedReminders {
+    static func load<T: Codable>() -> T? {
         if let url, let data = try? Data(contentsOf: url) {
-            (try? JSONDecoder().decode(CodedReminders.self, from: data)) ?? []
+            try? JSONDecoder().decode(T.self, from: data)
         } else {
-            []
+            nil
         }
     }
     
-    static func save(reminders: CodedReminders) {
-        guard let url, let data = try? JSONEncoder().encode(reminders) else { return }
+    static func save<T: Codable>(reminders: T) {
+        guard let url else { return }
+        
+        let encoder = JSONEncoder()
+        encoder.outputFormatting = [.prettyPrinted, .sortedKeys]
         do {
-            let json = try JSONSerialization.data(withJSONObject: data)
-            try json.write(to: url, options: [.atomic])
-        } catch {}
+            let data = try encoder.encode(reminders)
+            try data.write(to: url, options: [.atomic])
+        } catch let error {
+            err("Error serializing reminders:", error)
+        }
     }
 }
