d7ad284d59f82880d5cb3a4d163d02646dc7dc13
Nicclassy <lucanapoli122@gmail.com>
2025-02-14 22:03:21 +0100

    Add more elements to `ReminderCreationView`

--- RandomReminder/Views/ReminderCreationView.swift
+++ RandomReminder/Views/ReminderCreationView.swift
@@ -8,21 +8,36 @@
 import SwiftUI
 
 struct ReminderCreationView: View {
-    @StateObject private var reminderBuilder = ReminderBuilder()
+    @StateObject private var builder = ReminderBuilder()
+    @State private var recurringEnabled = false
 
     var body: some View {
         VStack(alignment: .leading, spacing: 20) {
             Text("Create new reminder")
                 .font(.title2)
             Text("Reminder title:")
-            TextField("Title", text: $reminderBuilder.title)
+            TextField("Title", text: $builder.title)
             
             Text("Reminder description:")
-            TextField("Description", text: $reminderBuilder.text)
+            TextField("Description", text: $builder.text)
             
-            VStack {
-                Stepper("Number of reminders: \(reminderBuilder.totalReminders)", value: $reminderBuilder.totalReminders, in: 1...100)
-                Toggle("Recurring", isOn: .constant(true))
+            Grid {
+                GridRow {
+                    Text("Number of reminders:")
+                    StepperTextField(value: $builder.totalReminders)
+                        .frame(width: 55)
+                    HStack(spacing: 0) {
+                        Toggle("Recurring every", isOn: $recurringEnabled)
+                        Picker("", selection: $builder.repeatInterval) {
+                            ForEach(RepeatInterval.allCases, id: \.self) { value in
+                                if value != .none {
+                                    Text(String(describing: value)).tag(value)
+                                }
+                            }
+                        }
+                        .disabled(!recurringEnabled)
+                    }
+                }
             }
         }
         .padding()
