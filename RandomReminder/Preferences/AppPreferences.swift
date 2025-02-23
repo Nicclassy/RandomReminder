65c70267fcf83e002a8ce61779dca71bec16051a
Nicclassy <lucanapoli122@gmail.com>
2025-02-23 11:36:42 +0100

    Repurpose `Preferences` for (non-)persistent preferences rather than views

--- RandomReminder/Preferences/AppPreferences.swift
+++ RandomReminder/Preferences/AppPreferences.swift
@@ -8,22 +8,14 @@
 import Foundation
 import SwiftUI
 
-enum Keys {
-    static let launchAtLogin = "launchAtLogin"
-    static let quickReminderEnabled = "quickReminderEnabled"
-    static let quickReminderStarted = "quickReminderStarted"
-    static let defaultEarliestTime = "defaultEarliestTime"
-    static let defaultLatestTime = "defaultLatestTime"
-}
-
 final class AppPreferences: ObservableObject {
     static let shared = AppPreferences()
     
     private init() {}
     
-    @AppStorage(Keys.launchAtLogin) var launchAtLogin = false
-    @AppStorage(Keys.quickReminderEnabled) var quickReminderEnabled = true
-    @AppStorage(Keys.quickReminderStarted) var quickReminderStarted = false
-    @AppStorage(Keys.defaultEarliestTime) var defaultEarliestTime: TimeInterval = 0
-    @AppStorage(Keys.defaultLatestTime) var defaultLatestTime: TimeInterval = 0
+    @AppStorage("launchAtLogin") var launchAtLogin = false
+    @AppStorage("quickReminderEnabled") var quickReminderEnabled = true
+    @AppStorage("quickReminderStarted") var quickReminderStarted = false
+    @AppStorage("defaultEarliestTime") var defaultEarliestTime: TimeInterval = 0
+    @AppStorage("defaultLatestTime") var defaultLatestTime: TimeInterval = 0
 }
---
+++ RandomReminder/Preferences/ReminderPreferences.swift
@@ -0,0 +1,23 @@
+//
+//  ReminderPreferences.swift
+//  RandomReminder
+//
+//  Created by Luca Napoli on 22/2/2025.
+//
+
+import Foundation
+import SwiftUI
+
+final class ReminderPreferences: ObservableObject {
+    @Published var repeatingEnabled = false
+    @Published var timesOnly = false
+    @Published var alwaysRunning = false
+    
+    @Published var specificDays = false
+    
+    @Published var showTimesOnlyPopover = false
+    @Published var showFileImporter = false
+    
+    @Published var earliestDate = Date()
+    @Published var latestDate = Date().addMinutes(60)
+}
