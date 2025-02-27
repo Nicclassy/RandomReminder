2c5a657359fdcf24426c7fa22cc96a432634e9d5
Nicclassy <lucanapoli122@gmail.com>
2025-02-27 21:42:35 +0100

    Fix bug of reminders not visually deleting

--- RandomReminder/Views/Preferences/RemindersPreferencesView.swift
+++ RandomReminder/Views/Preferences/RemindersPreferencesView.swift
@@ -14,23 +14,49 @@ struct ReminderPreferencesRow: View {
     
     @State private var reminder: RandomReminder
     @State private var reminderInfo: String
+    @State private var showDeleteAlert = false
+    
+    @Binding private var editing: Bool
     private var timer: PublishedTimer
     
-    init(reminder: RandomReminder, updateTimer timer: PublishedTimer) {
+    init(reminder: RandomReminder, updateTimer timer: PublishedTimer, editing: Binding<Bool>) {
         self.reminder = reminder
         self.reminderInfo = TimeInfoProvider(reminder: reminder).preferencesInfo()
         self.timer = timer
+        self._editing = editing
     }
     
     var body: some View {
         HStack {
             Text(reminder.content.title)
             Spacer()
-            Text(reminderInfo)
-                .foregroundStyle(.secondary)
-                .onReceive(timer) { _ in
-                    reminderInfo = TimeInfoProvider(reminder: reminder).preferencesInfo()
+            if editing {
+                Button("Edit") {}
+                Button("Delete") {
+                    showDeleteAlert = true
                 }
+                .alert(
+                    "Are you sure you want to delete the reminder '\(reminder.content.title)'?",
+                    isPresented: $showDeleteAlert
+                ) {
+                    Button("Delete", role: .destructive) {
+                        withAnimation {
+                            ReminderManager.shared.removeReminder(reminder)
+                        }
+                        FancyLogger.info("Deleted reminder '\(reminder.content.title)'")
+                    }
+                    Button("Cancel", role: .cancel) {}
+                } message: {
+                    Text("Deleted reminders cannot be recovered.")
+                }
+            } else {
+                Text(reminderInfo)
+                    .frame(height: 20)
+                    .foregroundStyle(.secondary)
+                    .onReceive(timer) { _ in
+                        reminderInfo = TimeInfoProvider(reminder: reminder).preferencesInfo()
+                    }
+            }
         }
         .padding(.all, 8)
         .padding(.horizontal, 2)
@@ -38,51 +64,87 @@ struct ReminderPreferencesRow: View {
 }
 
 struct ReminderPreferencesRows: View {
+    @Binding private var editingReminders: Bool
     private let remindersProvider: () -> [RandomReminder]
     private let timer = Timer.publish(every: 1, on: .current, in: .common).autoconnect()
+    private let heading: String
     
-    init(remindersProvider: @autoclosure @escaping () -> [RandomReminder]) {
+    init(heading: String, editingReminders: Binding<Bool>, remindersProvider: @autoclosure @escaping () -> [RandomReminder]) {
+        self.heading = heading
         self.remindersProvider = remindersProvider
+        self._editingReminders = editingReminders
     }
     
     var body: some View {
-        VStack(alignment: .leading) {
-            Divider()
-                .opacity(0)
-            let reminders = remindersProvider()
-            ForEach(reminders.enumeratedArray(), id: \.0) { index, reminder in
-                let dividerIsInvisible = index == reminders.endIndex - 1
-                ReminderPreferencesRow(reminder: reminder, updateTimer: timer)
-                Divider()
-                    .opacity(dividerIsInvisible ? 0 : 1)
+        let reminders = remindersProvider()
+        if !reminders.isEmpty {
+            VStack(alignment: .leading) {
+                Text(heading)
+                    .font(.headline)
+                    .padding(.bottom, 5)
+                VStack {
+                    Divider().opacity(0)
+                    ForEach(reminders.enumeratedArray(), id: \.1.id) { index, reminder in
+                        let dividerIsInvisible = index == reminders.endIndex - 1
+                        ReminderPreferencesRow(reminder: reminder, updateTimer: timer, editing: $editingReminders)
+                        Divider().opacity(dividerIsInvisible ? 0 : 1)
+                    }
+                }
+                .background(
+                    RoundedRectangle(cornerRadius: 10)
+                        .fill(.quaternary)
+                        .stroke(.quaternary, lineWidth: 1)
+                )
             }
         }
-        .background(
-            RoundedRectangle(cornerRadius: 10)
-                .fill(.quaternary)
-                .stroke(.quaternary, lineWidth: 1)
-        )
     }
 }
 
 struct RemindersPreferencesView: View {
     @StateObject var appPreferences: AppPreferences = .shared
-    @StateObject var reminderManager: ReminderManager = .preview
+    @StateObject var reminderManager: ReminderManager = .shared
+    @State private var editingReminders = false
     
     var body: some View {
         Settings.Container(contentWidth: 500) {
             Settings.Section(title: "") {
                 VStack(alignment: .leading) {
-                    Text("Upcoming Reminders")
-                        .font(.headline)
-                        .padding(.bottom, 5)
-                    ReminderPreferencesRows(remindersProvider: reminderManager.reminders.lazy.filter { !$0.hasPast() })
+                    ReminderPreferencesRows(
+                        heading: "Upcoming reminders",
+                        editingReminders: $editingReminders,
+                        remindersProvider: reminderManager.upcomingReminders()
+                    )
+                    .padding(.bottom, 10)
                     
-                    Text("Past Reminders")
-                        .font(.headline)
-                        .padding(.bottom, 5)
-                        .padding(.top, 10)
-                    ReminderPreferencesRows(remindersProvider: reminderManager.reminders.lazy.filter { $0.hasPast() })
+                    ReminderPreferencesRows(
+                        heading: "Past Reminders",
+                        editingReminders: $editingReminders,
+                        remindersProvider: reminderManager.pastReminders()
+                    )
+                }
+                .padding(.bottom, 15)
+                
+                HStack {
+                    if editingReminders {
+                        Button("Create New Reminder") {}
+                            .buttonStyle(.automatic)
+                            .disabled(true)
+                    } else {
+                        Button("Create New Reminder") {}
+                            .buttonStyle(.borderedProminent)
+                    }
+                    Spacer()
+                    if editingReminders {
+                        // TODO: Size buttons the same
+                        Button("Stop Editing") {
+                            editingReminders.toggle()
+                        }
+                        .buttonStyle(.borderedProminent)
+                    } else {
+                        Button("Edit Reminders") {
+                            editingReminders.toggle()
+                        }
+                    }
                 }
             }
         }
