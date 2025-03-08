6683807ecbc732e0f6d79a887ea90c354d3efe73
Nicclassy <lucanapoli122@gmail.com>
2025-03-08 15:04:03 +0100

    Use `Form` for general preferences

--- RandomReminder/Views/Preferences/GeneralPreferencesView.swift
+++ RandomReminder/Views/Preferences/GeneralPreferencesView.swift
@@ -12,20 +12,38 @@ struct GeneralPreferencesView: View {
     @StateObject var appPreferences: AppPreferences = .shared
     
     var body: some View {
-        VStack(alignment: .leading) {
-            Toggle(L10n.Preferences.General.launchAtLogin, isOn: appPreferences.$launchAtLogin)
+        Form {
+            Section {
+                Toggle(isOn: appPreferences.$launchAtLogin) {
+                    Text(L10n.Preferences.General.launchAtLogin)
+                    PreferenceCaption(L10n.Preferences.General.LaunchAtLogin.caption)
+                }
                 .onChange(of: appPreferences.launchAtLogin) { _, isEnabled in
                     LaunchAtLogin.isEnabled = isEnabled
                 }
-            PreferenceCaption(L10n.Preferences.General.LaunchAtLogin.caption)
-            
-            Toggle(L10n.Preferences.General.quickReminderEnabled, isOn: appPreferences.$quickReminderEnabled)
+            }
+
+            Section {
+                Toggle(isOn: appPreferences.$quickReminderEnabled) {
+                    Text(L10n.Preferences.General.quickReminderEnabled)
+                    PreferenceCaption(L10n.Preferences.General.QuickReminderEnabled.caption)
+                }
                 .onChange(of: appPreferences.quickReminderEnabled) { _, isEnabled in
                     AppDelegate.shared.toggleQuickReminder(isEnabled: isEnabled)
                 }
-            PreferenceCaption(L10n.Preferences.General.QuickReminderEnabled.caption)
+            }
+            
+            Section {
+                Toggle(isOn: appPreferences.$quickReminderEnabled) {
+                    Text("Show reminder counts")
+                    PreferenceCaption("Show the number of reminders in each category in the category's heading")
+                }
+                .onChange(of: appPreferences.showReminderCounts) { _, showReminderCount in
+                    appPreferences.showReminderCounts = showReminderCount
+                }
+            }
         }
-        .frame(width: 300, height: 250)
+        .frame(width: 320, height: 250)
     }
 }
 
