c0b5446e0d25a5e8691ceb170cb487106fc1517e
Nicclassy <lucanapoli122@gmail.com>
2025-04-10 21:10:30 +0200

    Add `ReminderModificationController`

---
+++ RandomReminder/Controllers/ReminderModificationController.swift
@@ -0,0 +1,14 @@
+//
+//  ReminderModificationController.swift
+//  RandomReminder
+//
+//  Created by Luca Napoli on 6/4/2025.
+//
+
+import Foundation
+
+final class ReminderModificationController {
+    static let shared = ReminderModificationController()
+    
+    weak var reminder: RandomReminder?
+}
