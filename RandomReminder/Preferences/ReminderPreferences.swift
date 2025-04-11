3ca732e45a9899e9ad82822818ca1b3eac68a111
Nicclassy <lucanapoli122@gmail.com>
2025-04-11 09:40:31 +0200

    Add reminder preference generation from reminders

--- RandomReminder/Preferences/ReminderPreferences.swift
+++ RandomReminder/Preferences/ReminderPreferences.swift
@@ -9,14 +9,45 @@ import Foundation
 import SwiftUI
 
 final class ReminderPreferences: ObservableObject {
-    @Published var repeatingEnabled = false
-    @Published var timesOnly = false
-    @Published var alwaysRunning = false
-    @Published var specificDays = false
-    @Published var useAudioFile = false
+    @Published var repeatingEnabled: Bool
+    @Published var timesOnly: Bool
+    @Published var alwaysRunning: Bool
+    @Published var specificDays: Bool
+    @Published var useAudioFile: Bool
     
     @Published var showSpecificDaysPopover = false
     @Published var showTimesOnlyPopover = false
     @Published var showCancelPopover = false
     @Published var showFileImporter = false
+    
+    init(
+        repeatingEnabled: Bool = false,
+        timesOnly: Bool = false,
+        alwaysRunning: Bool = false,
+        specificDays: Bool = false,
+        useAudioFile: Bool = false
+    ) {
+        self.repeatingEnabled = repeatingEnabled
+        self.timesOnly = timesOnly
+        self.alwaysRunning = alwaysRunning
+        self.specificDays = specificDays
+        self.useAudioFile = useAudioFile
+    }
+    
+    func from(reminder: RandomReminder) -> Self {
+        let alwaysRunning = if let interval = reminder.interval as? ReminderTimeInterval, interval.isInfinite {
+            true
+        } else {
+            false
+        }
+        let repeatingEnabled = !alwaysRunning && reminder.interval.repeatInterval != .never
+        let specificDays = !alwaysRunning && !reminder.days.isEmpty
+        let useAudioFile = reminder.activationEvents.audio != nil
+        return Self(
+            repeatingEnabled: repeatingEnabled,
+            alwaysRunning: alwaysRunning,
+            specificDays: specificDays,
+            useAudioFile: useAudioFile
+        )
+    }
 }
