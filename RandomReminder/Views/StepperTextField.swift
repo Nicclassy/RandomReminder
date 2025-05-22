ac37e554d85d76b6ea1b12e13a69d35964244c51
Nicclassy <lucanapoli122@gmail.com>
2025-05-22 18:25:12 +0200

    Add an option for a view between the stepper and the text field of `StepperTextField`

--- RandomReminder/Views/StepperTextField.swift
+++ RandomReminder/Views/StepperTextField.swift
@@ -11,8 +11,11 @@ import SwiftUI
 struct StepperTextField: View {
     @Binding private var value: Int
     @State private var text: String
+    @State private var stepperClicked = false
     let range: ClosedRange<Int>
     let spacing: CGFloat
+    let textFieldWidth: CGFloat
+    private let preStepperView: ((Int) -> AnyView)?
     private let onTextChange: ((String) -> Void)?
 
     var body: some View {
@@ -26,8 +29,12 @@ struct StepperTextField: View {
                 }
                 .onChange(of: text) { oldValue, newValue in
                     defer {
-                        FancyLogger.warn("Triggering onTextChange")
                         onTextChange?(text)
+                    }
+
+                    guard !stepperClicked else {
+                        stepperClicked = false
+                        return
                     }
 
                     guard !newValue.isEmpty else {
@@ -38,34 +45,52 @@ struct StepperTextField: View {
                     if let numericValue = Int(newValue) {
                         value = constrainValue(numericValue, orElse: value)
                         text = String(value)
-                    } else if oldValue.isEmpty {
-                        text = oldValue
+                    } else {
+                        text = if oldValue.isEmpty {
+                            oldValue
+                        } else {
+                            String(value)
+                        }
                     }
                 }
+                .frame(width: textFieldWidth)
+
+            if let preStepperView {
+                Spacer().frame(width: spacing)
+                preStepperView(value)
+            }
+
             Stepper(
                 "",
                 onIncrement: {
+                    stepperClicked = true
                     value = constrainValue(value + 1, orElse: value)
                     text = String(value)
                 },
                 onDecrement: {
+                    stepperClicked = true
                     value = constrainValue(value - 1, orElse: value)
                     text = String(value)
                 }
             )
+            .padding(.leading, -5)
         }
     }
 
     init(
         value: Binding<Int>,
         range: ClosedRange<Int> = Int.min...Int.max,
-        spacing: CGFloat = -5,
+        spacing: CGFloat = 0,
+        textFieldWidth: CGFloat = 35,
+        preStepperView: ((Int) -> AnyView)? = nil,
         onTextChange: ((String) -> Void)? = nil
     ) {
         self._value = value
         self._text = State(initialValue: String(value.wrappedValue))
         self.range = range
         self.spacing = spacing
+        self.textFieldWidth = textFieldWidth
+        self.preStepperView = preStepperView
         self.onTextChange = onTextChange
     }
 
@@ -76,19 +101,24 @@ struct StepperTextField: View {
 
 private struct StepperTextFieldPreview: View {
     @State private var value = 1
+    @State private var selection: RepeatInterval = .minute
     private let showNumber = false
 
     var body: some View {
         VStack {
-            StepperTextField(value: $value, range: 0...100)
-                .frame(width: 50)
+            StepperTextField(
+                value: $value,
+                range: 0...100,
+                spacing: 0,
+                textFieldWidth: 45
+            )
             if showNumber {
                 Text("Value is \(value)")
                     .font(.caption)
                     .foregroundStyle(.secondary)
             }
         }
-        .frame(width: 120, height: 120)
+        .frame(width: 240, height: 120)
         .padding()
     }
 }
