f161a16dfd790491f59918c58c1fb3cb33b520ae
Nicclassy <lucanapoli122@gmail.com>
2025-06-20 10:34:54 +0200

    Add timeout functionality to `Subprocess`

--- RandomReminder/Helpers/Subprocess.swift
+++ RandomReminder/Helpers/Subprocess.swift
@@ -7,73 +7,96 @@
 
 import Foundation
 
-enum SubprocessResult {
+enum SubprocessResult: Equatable {
     case success
     case error(String)
-    case timeout
+    case timeout(TimeInterval)
 }
 
 struct Subprocess {
     private static let defaultExecutableURL = URL(fileURLWithPath: "/bin/bash")
-    
+    private static let timeoutSeconds: TimeInterval = 1
+
     private let command: String
-    private var executed = false
     private var commandOutput: String?
     private var commandErrors: String?
-    
+    private var executed = false
+
     var stdout: String {
         guard executed else {
             fatalError("Command has not been executed")
         }
-        
+
         return commandOutput ?? ""
     }
-    
+
     var stderr: String {
         guard executed else {
             fatalError("Command has not been executed")
         }
-        
+
         return commandErrors ?? ""
     }
-    
+
     init(command: String) {
         self.command = command
     }
     
+    private static func withThrowingTimeout(
+        seconds: TimeInterval = timeoutSeconds,
+        operation: @escaping () throws -> Void
+    ) -> SubprocessResult {
+        let semaphore = DispatchSemaphore(value: 0)
+        var result: SubprocessResult = .success
+        
+        Task.detached {
+            do {
+                try operation()
+            } catch let error {
+                result = .error(error.localizedDescription)
+            }
+            semaphore.signal()
+        }
+        
+        let timeoutResult = semaphore.wait(timeout: .now() + seconds)
+        return timeoutResult == .timedOut ? .timeout(seconds) : result
+    }
+
     mutating func run() -> SubprocessResult {
         let process = Process()
         process.arguments = ["-c", command]
         process.executableURL = Self.defaultExecutableURL
-        
+
         let outputPipe = Pipe()
         let errorPipe = Pipe()
         process.standardOutput = outputPipe
         process.standardError = errorPipe
-        
+
         assert(!executed)
         defer {
             executed = true
             outputPipe.fileHandleForReading.closeFile()
             errorPipe.fileHandleForReading.closeFile()
         }
-        
-        do {
+
+        let result = Self.withThrowingTimeout {
             try process.run()
             process.waitUntilExit()
-        } catch let error {
-            return .error(error.localizedDescription)
         }
-        
+        if result != .success {
+            return result
+        }
+
         var outputData: Data?
         var errorData: Data?
+        
         do {
             outputData = try outputPipe.fileHandleForReading.readToEnd()
             errorData = try errorPipe.fileHandleForReading.readToEnd()
         } catch let error {
             return .error(error.localizedDescription)
         }
-        
+
         commandOutput = if let outputData {
             String(bytes: outputData, encoding: .utf8)
         } else {
@@ -84,7 +107,7 @@ struct Subprocess {
         } else {
             nil
         }
-        
+
         return .success
     }
 }
