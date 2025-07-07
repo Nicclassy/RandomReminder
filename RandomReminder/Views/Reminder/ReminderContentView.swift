740304f23f4f841f5fc68b8d83e69ca6f697c2f0
Nicclassy <lucanapoli122@gmail.com>
2025-07-07 13:14:19 +0200

    Move some of the reminder creation into `ReminderContentView`

---
+++ RandomReminder/Views/Reminder/ReminderContentView.swift
@@ -0,0 +1,72 @@
+//
+//  ReminderContentView.swift
+//  RandomReminder
+//
+//  Created by Luca Napoli on 7/7/2025.
+//
+
+import SwiftUI
+
+struct ReminderContentView: View {
+    @Environment(\.openWindow) private var openWindow
+
+    @ObservedObject var reminder: MutableReminder
+    @ObservedObject var fields: ModificationViewFields
+
+    var body: some View {
+        Grid(alignment: .leading) {
+            GridRow {
+                Text("Reminder title:")
+                TextField("Title", text: $reminder.title)
+            }
+            GridRow {
+                Text("Reminder description:")
+                HStack {
+                    if case let .text(description) = reminder.description {
+                        TextField(
+                            "Description",
+                            text: Binding(
+                                get: { description },
+                                set: { reminder.description = .text($0) }
+                            )
+                        )
+                    } else {
+                        Button("Delete description command") {
+                            reminder.description = .text("")
+                        }
+                    }
+                    Spacer()
+                    Button(
+                        action: {
+                            openWindow(id: WindowIds.descriptionCommand)
+                        },
+                        label: {
+                            Text("⌘")
+                        }
+                    )
+                }
+            }
+            GridRow {
+                Text("Total occurences:")
+                StepperTextField(
+                    value: $reminder.totalOccurences,
+                    range: reminderOccurencesRange,
+                    onTextChange: { text in
+                        DispatchQueue.main.async {
+                            fields.occurrencesText = text
+                        }
+                    }
+                )
+                .frame(width: 55)
+            }
+        }
+    }
+
+    private var reminderOccurencesRange: ClosedRange<Int> {
+        ReminderConstants.minOccurences...ReminderConstants.maxOccurences
+    }
+}
+
+#Preview {
+    ReminderContentView(reminder: .init(), fields: .init())
+}
