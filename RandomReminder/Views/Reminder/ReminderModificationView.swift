b41078d54c0a2450fb111c94e94c10f1774a7e99
Nicclassy <lucanapoli122@gmail.com>
2025-12-18 22:35:54 +1100

    Make reminder modification a multi-step process

--- RandomReminder/Views/Reminder/ReminderModificationView.swift
+++ RandomReminder/Views/Reminder/ReminderModificationView.swift
@@ -7,6 +7,11 @@
 
 import SwiftUI
 
+private enum ReminderModificationStep: Traversable {
+    case content
+    case create
+}
+
 enum ReminderModificationMode {
     case create
     case edit
@@ -22,41 +27,28 @@ final class ModificationViewFields: ObservableObject {
     }
 }
 
-struct ReminderModificationView: View {
+private struct ReminderContentView: View {
     @Environment(\.openWindow) private var openWindow
     @Environment(\.dismissWindow) private var dismissWindow
 
-    @State var reminder: MutableReminder = .init()
-    @StateObject var preferences: ReminderPreferences = .init()
-    @StateObject var viewPreferences: ModificationViewPreferences = .init()
-    @StateObject var fields: ModificationViewFields = .init()
-    @State private var validationResult: ValidationResult = .unset
+    @Binding var reminder: MutableReminder
+    @ObservedObject var preferences: ReminderPreferences
+    @ObservedObject var viewPreferences: ModificationViewPreferences
+    @ObservedObject var fields: ModificationViewFields
 
-    @ObservedObject private var controller: ReminderModificationController = .shared
-    private let schedulingPreferences: SchedulingPreferences = .shared
+    private let commandController: CommandController = .shared
     let mode: ReminderModificationMode
+    let onNextButtonClicked: () -> Void
 
     var body: some View {
-        // swiftlint:disable:next closure_body_length
-        VStack(alignment: .leading, spacing: 20) {
+        VStack(alignment: .leading) {
             ReminderContentOptionsView(
                 reminder: reminder,
                 preferences: preferences,
                 fields: fields
             )
 
-            ReminderOptionsView(
-                reminder: reminder,
-                preferences: preferences,
-                viewPreferences: viewPreferences,
-                fields: fields
-            )
-
-            ReminderDateView(
-                reminder: reminder,
-                preferences: preferences
-            )
-
+            Spacer().frame(height: ViewConstants.optionsSpacing)
             ReminderAudioOptionsView(
                 reminder: reminder,
                 preferences: preferences,
@@ -64,127 +56,72 @@ struct ReminderModificationView: View {
                 useAudioFile: $preferences.useAudioFile
             )
 
+            HStack(spacing: ViewConstants.horizontalSpacing) {
+                Toggle(
+                    "Run a command when the reminder appears",
+                    isOn: $reminder.activationEvents.command.isEnabled
+                )
+                Button(reminder.activationEvents.command.value.isEmpty ? "Choose command" : "Edit command") {
+                    commandController.set(value: reminder.activationEvents.command.value, for: .activationCommand)
+                    commandController.commandType = .activationCommand
+                    openWindow(id: WindowIds.reminderCommand)
+                }
+                .disabled(!reminder.activationEvents.command.isEnabled)
+            }
+
+            Spacer()
             HStack {
-                Button(action: {
-                    let validator = ReminderValidator(
-                        reminder: reminder,
-                        preferences: preferences,
-                        fields: fields
-                    )
-
-                    validationResult = validator.validate()
-                    switch validationResult {
-                    case .success, .unset:
-                        break
-                    case .error, .warning:
-                        viewPreferences.showReminderAlert = true
-                        return
-                    }
-
-                    createNewReminder()
-                    if mode == .edit {
-                        NotificationCenter.default.post(name: .updateReminderPreferencesText, object: nil)
-                    }
-                    dismissWindow(id: mode == .create ? WindowIds.createReminder : WindowIds.editReminder)
-                }, label: {
-                    Text(finishButtonText)
-                        .frame(width: 60)
-                })
-                .alert(
-                    validationResult.alertText,
-                    isPresented: $viewPreferences.showReminderAlert,
-                    actions: {
-                        if case .error = validationResult {
-                            Button(L10n.Modification.ok) {}
-                                .buttonStyle(.borderedProminent)
-                        } else if case .warning = validationResult {
-                            Button(L10n.Modification.cancel, role: .cancel) {}
-                            Button(L10n.Modification.ok) {
-                                createNewReminder()
-                                viewPreferences.closeView = true
-                            }
-                        }
-                    },
-                    message: {
-                        Text(validationResult.messageText)
+                cancelButton
+                Spacer()
+                Button(
+                    action: onNextButtonClicked,
+                    label: {
+                        Text("Next")
+                            .frame(width: ViewConstants.modificationButtonSize)
                     }
                 )
                 .disabled(reminder.title.isEmpty)
                 .if(!reminder.title.isEmpty) { it in
                     it.buttonStyle(.borderedProminent)
                 }
+            }
+        }
+    }
 
-                Button(action: {
-                    viewPreferences.showCancelAlert = true
-                }, label: {
-                    Text(L10n.Modification.cancel)
-                        .frame(width: 60)
-                })
-                .alert(
-                    alertTitle,
-                    isPresented: $viewPreferences.showCancelAlert,
-                    actions: {
-                        Button(L10n.Modification.cancel, role: .cancel) {}
-                        Button(L10n.Modification.delete, role: .destructive) {
-                            viewPreferences.closeView = true
-                        }
-                    },
-                    message: {
-                        Text(L10n.Modification.DiscardReminder.message)
-                    }
-                )
-                .onChange(of: viewPreferences.closeView) {
-                    // For some reason, dismissing the window after the alert has closed
-                    // or in this closure does not work.
-                    // Therefore, it is necessary to close the window itself instead
-                    if viewPreferences.closeView {
-                        NSApp.keyWindow?.close()
-                        dismissWindow(id: WindowIds.descriptionCommand)
-                        viewPreferences.closeView = false
-                    }
+    @ViewBuilder
+    var cancelButton: some View {
+        Button(
+            action: {
+                viewPreferences.showCancelAlert = true
+            },
+            label: {
+                Text(L10n.Modification.cancel)
+                    .frame(width: ViewConstants.modificationButtonSize)
+            }
+        )
+        .alert(
+            alertTitle,
+            isPresented: $viewPreferences.showCancelAlert,
+            actions: {
+                Button(L10n.Modification.cancel, role: .cancel) {}
+                Button(L10n.Modification.delete, role: .destructive) {
+                    viewPreferences.closeView = true
                 }
+            },
+            message: {
+                Text(L10n.Modification.DiscardReminder.message)
+            }
+        )
+        .onChange(of: viewPreferences.closeView) {
+            // For some reason, dismissing the window after the alert has closed
+            // or in this closure does not work.
+            // Therefore, it is necessary to close the window itself instead
+            if viewPreferences.closeView {
+                NSApp.keyWindow?.close()
+                dismissWindow(id: WindowIds.reminderCommand)
+                viewPreferences.closeView = false
             }
         }
-        .onAppear {
-            guard mode == .edit else {
-                setDefaultTimes()
-                return
-            }
-
-            guard let reminderToEdit = controller.reminder else {
-                fatalError("Reminder must be set")
-            }
-
-            reminder.copyFrom(reminder: reminderToEdit)
-            preferences.copyFrom(reminder: reminderToEdit)
-
-            let description = reminderToEdit.content.description
-            if case .command = description {
-                controller.editDescriptionCommand(description)
-            }
-        }
-        .onDisappear {
-            reminder.reset()
-            preferences.reset()
-            fields.reset()
-            dismissWindow(id: WindowIds.descriptionCommand)
-            controller.modificationWindowOpen = false
-        }
-        .onReceive(NotificationCenter.default.publisher(for: .refreshModificationWindow)) { _ in
-            FancyLogger.info("Refresh modification window toggled")
-            viewPreferences.refreshView.toggle()
-        }
-        .onReceive(NotificationCenter.default.publisher(for: .descriptionCommandSet)) { _ in
-            reminder.description = controller.descriptionCommand
-        }
-        .onReceive(NotificationCenter.default.publisher(for: .openActiveReminderWindow)) { _ in
-            openWindow(id: WindowIds.activeReminder)
-        }
-        .onReceive(NotificationCenter.default.publisher(for: .dismissActiveReminderWindow)) { _ in
-            dismissWindow(id: WindowIds.activeReminder)
-        }
-        .frame(width: ViewConstants.reminderWindowWidth, height: ViewConstants.reminderWindowHeight)
-        .padding()
     }
 
     private var alertTitle: String {
@@ -194,9 +131,180 @@ struct ReminderModificationView: View {
             L10n.Modification.DiscardReminder.Edit.title
         }
     }
+}
+
+private struct ReminderCreateView: View {
+    @Environment(\.openWindow) private var openWindow
+    @Environment(\.dismissWindow) private var dismissWindow
+
+    @Binding var reminder: MutableReminder
+    @Binding var validationResult: ValidationResult
+    @ObservedObject var preferences: ReminderPreferences
+    @ObservedObject var viewPreferences: ModificationViewPreferences
+    @ObservedObject var fields: ModificationViewFields
+    private let schedulingPreferences: SchedulingPreferences = .shared
+
+    let mode: ReminderModificationMode
+    let createNewReminder: () -> Void
+    let onCreateButtonClicked: () -> Void
+    let onBackButtonClicked: () -> Void
+
+    var body: some View {
+        VStack(alignment: .leading) {
+            VStack(alignment: .leading, spacing: 10) {
+                ReminderDateView(
+                    reminder: reminder,
+                    preferences: preferences
+                )
+
+                ReminderOptionsView(
+                    reminder: reminder,
+                    preferences: preferences,
+                    viewPreferences: viewPreferences,
+                    fields: fields
+                )
+            }
+
+            Spacer()
+            HStack {
+                Button(
+                    action: onBackButtonClicked,
+                    label: {
+                        Text("Back")
+                            .frame(width: ViewConstants.modificationButtonSize)
+                    }
+                )
+                Spacer()
+                createButton
+            }
+        }
+    }
+
+    @ViewBuilder
+    var createButton: some View {
+        Button(
+            action: onCreateButtonClicked,
+            label: {
+                Text(finishButtonText)
+                    .frame(width: ViewConstants.modificationButtonSize)
+            }
+        )
+        .buttonStyle(.borderedProminent)
+        .alert(
+            validationResult.alertText,
+            isPresented: $viewPreferences.showReminderAlert,
+            actions: {
+                if case .error = validationResult {
+                    Button(L10n.Modification.ok) {}
+                        .buttonStyle(.borderedProminent)
+                } else if case .warning = validationResult {
+                    Button(L10n.Modification.cancel, role: .cancel) {}
+                    Button(L10n.Modification.ok) {
+                        createNewReminder()
+                        viewPreferences.closeView = true
+                    }
+                }
+            },
+            message: {
+                Text(validationResult.messageText)
+            }
+        )
+    }
 
     private var finishButtonText: String {
         mode == .create ? L10n.Modification.create : L10n.Modification.save
+    }
+}
+
+struct ReminderModificationView: View {
+    @Environment(\.openWindow) private var openWindow
+    @Environment(\.dismissWindow) private var dismissWindow
+
+    @State var reminder: MutableReminder = .init()
+    @State private var validationResult: ValidationResult = .unset
+    @State private var step: ReminderModificationStep = .content
+    @StateObject var preferences: ReminderPreferences = .init()
+    @StateObject var viewPreferences: ModificationViewPreferences = .init()
+    @StateObject var fields: ModificationViewFields = .init()
+    @ObservedObject private var controller: ReminderModificationController = .shared
+
+    private let schedulingPreferences: SchedulingPreferences = .shared
+    private let commandController: CommandController = .shared
+
+    let mode: ReminderModificationMode
+
+    var body: some View {
+        activeView
+            .onAppear {
+                guard mode == .edit else {
+                    setDefaultTimes()
+                    return
+                }
+
+                guard let reminderToEdit = controller.reminder else {
+                    fatalError("Reminder must be set")
+                }
+
+                reminder.copyFrom(reminder: reminderToEdit)
+                preferences.copyFrom(reminder: reminderToEdit)
+
+                let description = reminderToEdit.content.description
+                if case .command = description {
+                    commandController.set(value: description, for: .descriptionCommand)
+                }
+            }
+            .onDisappear {
+                reminder.reset()
+                preferences.reset()
+                fields.reset()
+                dismissWindow(id: WindowIds.reminderCommand)
+                controller.modificationWindowOpen = false
+            }
+            .onReceive(NotificationCenter.default.publisher(for: .refreshModificationWindow)) { _ in
+                viewPreferences.refreshView.toggle()
+            }
+            .onReceive(.descriptionCommand) { _ in
+                reminder.description = commandController.value(for: .descriptionCommand)
+            }
+            .onReceive(NotificationCenter.default.publisher(for: .openActiveReminderWindow)) { _ in
+                openWindow(id: WindowIds.activeReminder)
+            }
+            .onReceive(NotificationCenter.default.publisher(for: .dismissActiveReminderWindow)) { _ in
+                dismissWindow(id: WindowIds.activeReminder)
+            }
+            .navigationTitle(step == .content ? "Reminder content" : "Reminder settings")
+            .frame(width: ViewConstants.reminderWindowWidth, height: ViewConstants.reminderWindowHeight)
+            .padding()
+    }
+
+    @ViewBuilder
+    private var activeView: some View {
+        if step == .content {
+            ReminderContentView(
+                reminder: $reminder,
+                preferences: preferences,
+                viewPreferences: viewPreferences,
+                fields: fields,
+                mode: mode,
+                onNextButtonClicked: {
+                    step.forward()
+                }
+            )
+        } else {
+            ReminderCreateView(
+                reminder: $reminder,
+                validationResult: $validationResult,
+                preferences: preferences,
+                viewPreferences: viewPreferences,
+                fields: fields,
+                mode: mode,
+                createNewReminder: createNewReminder,
+                onCreateButtonClicked: create,
+                onBackButtonClicked: {
+                    step.backward()
+                }
+            )
+        }
     }
 
     private func setDefaultTimes() {
@@ -207,6 +315,29 @@ struct ReminderModificationView: View {
             reminder.latestDate = .dateToday(withTime: schedulingPreferences.defaultLatestTime)
             FancyLogger.info("Latest date: \(reminder.latestDate)")
         }
+    }
+
+    private func create() {
+        let validator = ReminderValidator(
+            reminder: reminder,
+            preferences: preferences,
+            fields: fields
+        )
+
+        validationResult = validator.validate()
+        switch validationResult {
+        case .success, .unset:
+            break
+        case .error, .warning:
+            viewPreferences.showReminderAlert = true
+            return
+        }
+
+        createNewReminder()
+        if mode == .edit {
+            NotificationCenter.default.post(name: .updateReminderPreferencesText, object: nil)
+        }
+        dismissWindow(id: mode == .create ? WindowIds.createReminder : WindowIds.editReminder)
     }
 
     private func createNewReminder() {
@@ -226,5 +357,10 @@ struct ReminderModificationView: View {
 }
 
 #Preview {
-    ReminderModificationView(reminder: .init(), preferences: .init(), viewPreferences: .init(), mode: .create)
+    ReminderModificationView(
+        reminder: .init(),
+        preferences: .init(),
+        viewPreferences: .init(),
+        mode: .create
+    )
 }
