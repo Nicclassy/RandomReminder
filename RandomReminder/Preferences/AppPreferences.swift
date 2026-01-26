50dda370ced757f0a018d292a6cca62724e9d582
Nicclassy <lucanapoli122@gmail.com>
2026-01-26 23:21:45 +1100

    Remove quick reminder settings

--- RandomReminder/Preferences/AppPreferences.swift
+++ RandomReminder/Preferences/AppPreferences.swift
@@ -14,8 +14,6 @@ final class AppPreferences: ObservableObject {
     @AppStorage("launchAtLogin") var launchAtLogin = false
     @AppStorage("singleModificationView") var singleModificationView = false
     @AppStorage("showReminderCounts") var showReminderCounts = true
-    @AppStorage("quickReminderEnabled") var quickReminderEnabled = true
-    @AppStorage("quickReminderStarted") var quickReminderStarted = false
     @AppStorage("randomiseAudioPlaybackStart") var randomiseAudioPlaybackStart = true
     @AppStorage("timeFormat") var timeFormat: TimeFormat = .long
     @AppStorage("allowBanners") var allowBanners = false
