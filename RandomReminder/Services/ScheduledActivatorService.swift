c5d5f769dd2f37bbd39a3bc89c22c4f0d472c5e1
Nicclassy <lucanapoli122@gmail.com>
2026-01-27 22:37:35 +1100

    Use guaranteed reminder activation via scheduling

---
+++ RandomReminder/Services/ScheduledActivatorService.swift
@@ -0,0 +1,80 @@
+//
+//  ScheduledActivatorService.swift
+//  RandomReminder
+//
+//  Created by Luca Napoli on 27/1/2026.
+//
+
+import Foundation
+
+private func calculateActivationDates(for reminder: RandomReminder) -> [Date] {
+    // Dates are spaced evenly; this activator is spaced and scheduled
+    let startDate = max(reminder.interval.earliest, .now)
+    let timeRemaining = reminder.interval.latest.timeIntervalSince(startDate)
+    let activationDuration = timeRemaining / TimeInterval(reminder.counts.occurencesLeft)
+
+    return (0..<reminder.counts.occurences).map { activationNumber in
+        let start = startDate + activationDuration * TimeInterval(activationNumber)
+        let end = start + activationDuration
+        return Date(
+            timeIntervalSince1970: .random(in: start.timeIntervalSince1970..<end.timeIntervalSince1970)
+        )
+    }
+}
+
+final class ScheduledActivatorService: ReminderActivatorService {
+    let reminder: RandomReminder
+    var running = false
+    var terminated = false
+
+    private let onReminderActivation: OnReminderActivation
+    private let onReminderFinished: OnReminderFinished
+    private var activationDates: [Date]
+    private var reminderActivations: Int
+
+    init(
+        reminder: RandomReminder,
+        onReminderActivation: @escaping OnReminderActivation,
+        onReminderFinished: @escaping OnReminderFinished
+    ) {
+        self.reminder = reminder
+        self.onReminderActivation = onReminderActivation
+        self.onReminderFinished = onReminderFinished
+        self.activationDates = calculateActivationDates(for: reminder)
+        self.reminderActivations = reminder.counts.occurences
+    }
+
+    func tick() {
+        let upcomingDate = activationDates[0]
+        guard !terminated else {
+            FancyLogger.info("Activator for '\(reminder)' has been terminated")
+            return
+        }
+
+        guard Date() >= upcomingDate else {
+            FancyLogger.info("Did not activate '\(reminder)'")
+            return
+        }
+
+        let isFinalActivation = reminderActivations == reminder.counts.totalOccurences - 1
+        if isFinalActivation {
+            reminderActivations += 1
+            FancyLogger.info(
+                "Activated final reminder for '\(reminder)'",
+                "(\(reminderActivations)/\(reminder.counts.totalOccurences))"
+            )
+            onReminderActivation()
+            onReminderFinished()
+            running = false
+        } else {
+            reminderActivations += 1
+            onReminderActivation()
+            FancyLogger.info(
+                "Activated '\(reminder)'",
+                "(\(reminderActivations)/\(reminder.counts.totalOccurences))!"
+            )
+        }
+
+        activationDates.removeFirst()
+    }
+}
---
+++ RandomReminder/Services/TickedActivatorService.swift
@@ -0,0 +1,76 @@
+//
+//  TickedActivatorService.swift
+//  RandomReminder
+//
+//  Created by Luca Napoli on 27/1/2026.
+//
+
+import Foundation
+
+final class TickedActivatorService: ReminderActivatorService {
+    let reminder: RandomReminder
+    var running = false
+    var terminated = false
+
+    private var reminderActivations: Int
+    private let activationProbability: Float
+
+    private let onReminderActivation: OnReminderActivation
+    private let onReminderFinished: OnReminderFinished
+
+    init(
+        reminder: RandomReminder,
+        every interval: ReminderTickInterval,
+        onReminderActivation: @escaping OnReminderActivation,
+        onReminderFinished: @escaping OnReminderFinished
+    ) {
+        self.reminder = reminder
+        // Note: we cannot trust the value of reminder.counts.occurences
+        // for determining how many activations we must do (except for determining this value initially)
+        // because this value (reminder occurences) is incremented
+        // when the notification disappears (as of now)
+        self.reminderActivations = reminder.counts.occurences
+        self.onReminderActivation = onReminderActivation
+        self.onReminderFinished = onReminderFinished
+
+        self.activationProbability = (
+            Float(reminder.counts.totalOccurences) * Float(interval.seconds()) / Float(reminder.durationInSeconds())
+        )
+        let endRemindersDate = reminder.interval.latest
+        FancyLogger.info("Reminder '\(reminder)' ends at \(endRemindersDate)")
+    }
+
+    func tick() {
+        guard !terminated else {
+            FancyLogger.info("Activator for '\(reminder)' has been terminated")
+            return
+        }
+
+        let reminderCanActivate = ReminderManager.shared.reminderCanActivate(reminder)
+        let reminderWillActivate = reminderCanActivate && reminderWillActivate()
+        let isFinalActivation = reminderActivations == reminder.counts.totalOccurences - 1
+        if reminderWillActivate && isFinalActivation {
+            reminderActivations += 1
+            FancyLogger.info(
+                "Activated final reminder for '\(reminder)'",
+                "(\(reminderActivations)/\(reminder.counts.totalOccurences))"
+            )
+            onReminderActivation()
+            onReminderFinished()
+            running = false
+        } else if reminderWillActivate {
+            reminderActivations += 1
+            onReminderActivation()
+            FancyLogger.info(
+                "Activated '\(reminder)'",
+                "(\(reminderActivations)/\(reminder.counts.totalOccurences))!"
+            )
+        } else {
+            FancyLogger.info("Did not activate '\(reminder)'")
+        }
+    }
+
+    private func reminderWillActivate() -> Bool {
+        Float.random(in: 0...1) < activationProbability
+    }
+}
